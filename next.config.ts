db33a4a3e88f277e83aa48f7e20d6c108b304ce0
KangEunTaek <ket3613@naver.com>
2025-10-17 09:25:28 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export',          // next export 사용
+    // output: 'export',  // ← 이 줄 제거
+  output: 'standalone', // 경량 서버 번들
   images: { unoptimized: true }, // 외부 이미지 최적화 비활성화
   trailingSlash: true        // Nginx 정적 호스팅에 유리
   // rewrites/redirects/middleware/API routes 사용 금지
